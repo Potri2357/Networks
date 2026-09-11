226e08be40e473f88343de92075f8163a92cf088
Potrinathan P M <potrinathan14@gmail.com>
2026-09-11 08:19:11 +0530

    Add Sliding Window client to compact folder

---
+++ sw_sr/client.py
@@ -0,0 +1,40 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1", 5000))
+
+print("Client connected to server")
+
+total_packets = 8
+window_size = 4
+lost_packet = 2
+start = 1
+
+while start <= total_packets:
+    end = min(start + window_size - 1, total_packets)
+    print("\nReceiving:", start, "to", end)
+
+    acks = ""
+
+    for i in range(start, end + 1):
+        if i == lost_packet:
+            continue
+
+        data = client.recv(1024).decode()
+        print("Received Packet:", data)
+        acks = acks + "ACK " + data + " "
+
+    print("Sending:", acks)
+    client.send(acks.encode())
+
+    if lost_packet >= start and lost_packet <= end:
+        data = client.recv(1024).decode()
+        print("Received Retransmitted Packet:", data)
+        client.send(("ACK " + data).encode())
+        lost_packet = -1
+
+    start = end + 1
+
+client.close()
+print("\nAll packets received")
+print("Client closed")
